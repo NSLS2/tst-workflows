cae6f6ad9fb86171dee3dea3ad58a0d626dd0ec1
Jun Aishima <jaishima@bnl.gov>
2026-02-24 18:19:13 -0500

    modify test to use prod Prefect instead of test harness

--- test_run.py
+++ test_run.py
@@ -1,6 +1,7 @@
 from prefect.logging import disable_run_logger
 from prefect.testing.utilities import prefect_test_harness
 from end_of_run_workflow import end_of_run_workflow
+import pytest
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -9,11 +10,7 @@ def prefect_disable_logging():
         yield
 
 
-@pytest.fixture(autouse=True, scope="session")
-def prefect_test_fixture():
-    with prefect_test_harness():
-        yield
-
-def test_end_of_run_workflow():
-    with prefect_test_harness():
-        assert end_of_run_workflow(stop_doc={"run_start":"f0954c84-f652-4350-9f6d-44b724f4ed9f"})
+def test_end_of_run_workflow(prefect_disable_logging):
+    print("starting test!")
+    assert end_of_run_workflow(stop_doc={"run_start":"f0954c84-f652-4350-9f6d-44b724f4ed9f"})
+    print("finished test!")
